55137b364c93961714be8250ea8f64239b50863f
pilot-a380 <110840866+pilot-a380@users.noreply.github.com>
2022-08-12 19:55:05 +0530

    Add files via upload

---
+++ character.py
@@ -0,0 +1,12 @@
+#Python project to find the type of character 
+#input the character
+
+
+character = input("Please input any character: ")
+if((character >= 'a' and character <= 'z' )) or ((character >= 'A' and character <= 'Z')):
+    print("The Given Character is a Alphabet")
+elif((character >= '0' and character <= '9')):
+    print("The Given Character Is a Number ")
+else:
+    print("The Given Character is a special character ")
+
