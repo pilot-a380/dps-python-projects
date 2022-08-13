2f85473317b23123c86c1f52e501e07692556cf7
pilot-a380 <110840866+pilot-a380@users.noreply.github.com>
2022-08-13 21:58:22 +0530

    Add files via upload

---
+++ finding the sum of first n natural numbers.py
@@ -0,0 +1,14 @@
+#Finding the sum of first N natural numbers
+def findSum(n) :
+    sum = 0
+    x = 1
+    while x <=n :
+        sum = sum + x
+        x = x + 1
+    return sum
+ 
+ 
+# Driver code
+ 
+n = 5
+print (findSum(n))
