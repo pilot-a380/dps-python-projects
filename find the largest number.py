5b3b0c054ce7037c362c71e2ad88647c81176db9
pilot-a380 <110840866+pilot-a380@users.noreply.github.com>
2022-08-13 21:29:55 +0530

    Add files via upload

---
+++ find the largest number.py
@@ -0,0 +1,14 @@
+num1 = int(input("Please Enter The First Number: "))
+num2 = int(input("Please Enter The Second Number: "))
+num3 = int(input("Please Enter The Third Number: "))
+
+# Check If number 1 is the largest one entered 
+
+if (num1>num2 and num1>num3):
+    print("Number 1 is the largest number")
+
+elif (num2>num1 and num2>num3):
+    print("Number 2 is the largest number")
+
+else:
+    print("Number 3 is the largest number ")
